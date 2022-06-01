5a3aeccd2e22604609e2fb1fd45b262f70ad12dd
sunxiao <1262632798@qq.com>
2022-06-01 08:53:56 -0700

    add 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,6 +6,7 @@ int main(void)
     cout<<"please not steal my food!!!"<<endl;
     cout<<"i feel so bad!!"<<endl;
     cout<<"i need to be strong,and trust myself!"<<endl;
+    printf("jahahahahaha\n");
 
     return 0;
 }
